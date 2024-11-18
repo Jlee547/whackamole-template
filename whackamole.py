82ee6c50d176f4000c1e0ec72d308cf9fa9f982e
Your Name <you@example.com>
2024-11-18 16:07:25 -0500

    whackamole.py

--- whackamole.py
+++ whackamole.py
@@ -1,20 +1,31 @@
 import pygame
-
+import random
 
 def main():
+    
     try:
         pygame.init()
-        # You can draw the mole with this snippet:
-        # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
         running = True
+        molX = 0
+        molY = 0
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    clickX, clickY = event.pos
+                    if clickX//32 == molX and clickY//32 == molY:
+                        molX = random.randrange(0, 20)
+                        molY = random.randrange(0, 16)
             screen.fill("light green")
+            screen.blit(mole_image, mole_image.get_rect(topleft=(molX * 32, molY * 32)))
+            for i in range(20):
+                pygame.draw.line(screen, (255,255,255), (32*i,0), (32*i,512))
+            for i in range(16):
+                pygame.draw.line(screen, (255,255,255), (0,32*i), (640,32*i))
             pygame.display.flip()
             clock.tick(60)
     finally:
